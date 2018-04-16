c785125f63bd99cb5c03e9dcc3e9d2c2f2c5279c
AlexanderTarasov11-708 <aleksandrtarasov53@gmail.com>
2018-04-16 13:31:27 +0300

    fix: Вторая семестровка

---
+++ 2017/SPRING/PS/PS2/Patience Sorting/PatienceSorting/PatienceSorting.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatienceSorting
+{
+    //класс с терпеливой сортировкой
+    class PatienceSorting
+    {
+        public int iterations;
+        //список стопок
+        private List<Pile> piles;
+
+        //конструктор
+        public PatienceSorting()
+        {
+            piles = new List<Pile>();
+            iterations = 0;
+        }
+
+        //сортировка в аргументе массив целых чисел
+        public List<Pile> Sort(int[] array)
+        {
+            //поэлементное добавление
+            foreach (var item in array)
+                PushItem(item);
+            return piles;
+        }
+
+        //добавление нового элемента в список
+        public void PushItem(int x)
+        {
+            //если список пуст, добавляем в него новую стопку
+            if (piles.Count == 0)
+                AddNewPile(x);
+            else
+            {
+                var flag = false;
+                var i = 0;
+                while (!flag)
+                {
+                    //если переходим на новую стопку в списке
+                    if (i == piles.Count)
+                    {
+                        AddNewPile(x);
+                        flag = true;
+                    }
+                    else
+                    {
+                        //если первое значение в стопке <= данного, спускаемся по этой стопке
+                        if (x <= piles[i].Head.Value)
+                        {
+                            for (int j = 0; j < piles[i].Length(); j++)
+                            {
+                                //если значение <= данного и (>= следующего или данное последнее в стопке), добавляем
+                                if ((x <= piles[i].GetNode(j).Value &&
+                                    ((piles[i].Length() == j + 1) || x >= piles[i].GetNode(j + 1).Value))
+                                        || piles[i].GetNode(j).Next == null)
+                                {
+                                    iterations++;
+                                    piles[i].Add(j + 1, x);
+                                    flag = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    i++;
+                }
+            }
+
+        }
+
+        //создание новой стопки по значению
+        private void AddNewPile(int x)
+        {
+            var queue = new Pile(x);
+            //добавление в список стопок
+            piles.Add(queue);
+        }
+
+        //вывод в массив
+        public int[] ToArray()
+        {
+            int count = 0;
+            int i = 0;
+            foreach (var elem in piles)
+                count += elem.Length();
+            var array = new int[count];
+            foreach (var elem in piles)
+                for (int j = elem.Length() - 1; j >= 0; j--)
+                    array[i++] = elem.ElementAt(j).Value;
+            return array;
+        }
+    }
+}
---
+++ 2017/SPRING/PS/PS2/Patience Sorting/PatienceSorting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace PatienceSorting
+{
+    public class Program
+    {
+        //проверка
+        static void Main(string[] args)
+        {
+            var readPath = new StreamReader("data.txt");
+
+            try
+            {
+                var array = DecodeText(readPath.ReadLine());
+                var sorting = new PatienceSorting();
+                DateTime time = DateTime.Now;
+                var sortedArray = sorting.Sort(array).ToArray();
+                //время работы программы
+                var measureTime = (DateTime.Now - time).TotalMilliseconds;  
+                //кол-во итераций
+                var sortIterations = sorting.iterations;
+                readPath.Close();
+            }
+            // если неправильные входные данные вывести сообщение
+            catch (FormatException)
+            {
+                Console.WriteLine("Во входном текстовом файле должны быть числа");
+            }
+        }
+
+        // Обработка ввода из текстового файла
+        public static int[] DecodeText(string path)
+        {
+            //разделение по пробелам
+            var stringNumbers = path.Split(' ');
+            var result = new int[stringNumbers.Length];
+            //парсинг по элементам
+            for (int i = 0; i < result.Length; i++)
+                result[i] = int.Parse(stringNumbers[i]);
+            return result;
+        }
+    }
+}
