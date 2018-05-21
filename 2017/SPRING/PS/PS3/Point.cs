5550dec7948e0d447d3cd1661f4d6d458b7850b2
AlexanderTarasov11-708 <aleksandrtarasov53@gmail.com>
2018-05-21 10:42:29 +0300

    fix: не отправил при заливке файл с классом Point

---
+++ 2017/SPRING/PS/PS3/Point.cs
@@ -0,0 +1,14 @@
+namespace ConvexHull
+{
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
